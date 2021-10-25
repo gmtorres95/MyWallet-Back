81fb532bd9acf412a1082ac9ac86ba9f09320e6f
Gabriel Torres <al2012.163.torres@gmail.com>
2021-10-25 01:30:31 -0300

    Implemented sign-up route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 
+import signUp from "./functions/signUp.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post("/sign-up", signUp);
 
 export default app;
